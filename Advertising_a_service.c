39a703c2c239946171026e0df4bc7885905a5fbb
unknown <david.fu@PC-01749.embed.com>
2017-09-11 15:38:20 +0800

    Fix some functions spell errors
    Signed-off-by: unknown <david.fu@PC-01749.embed.com>

--- Advertising_a_service.c
+++ Advertising_a_service.c
@@ -24,50 +24,50 @@ sdp_session_t * register_service()
 	sdp_record_t record={0};
 	sdp_session_t * session=0;
 	//setthegeneralserviceID
-	sdpuuid128create(&svc_uuid,&svc_uuid_int);
+	sdp_uuid128_create(&svc_uuid,&svc_uuid_int);
 	sdp_set_service_id(&record,svc_uuid);
 	//settheserviceclass
 	sdp_uuid16_create(&svc_class_uuid,SERIAL_PORT_SVCLASS_ID);
-	svc_class_list=sdplistappend(0,&svc_class_uuid);
-	sdpsetserviceclasses(&record,svc_class_list);
+	svc_class_list=sdp_list_append(0,&svc_class_uuid);
+	sdp_set_service_classes(&record,svc_class_list);
 	//settheBluetoothprofileinformation
 	sdp_uuid16_create(&profile.uuid,SERIAL_PORT_PROFILE_ID);
 	profile.version=0x0100;
-	profile_list=sdplistappend(0,&profile);
-	sdpsetprofiledescs(&record,profile_list);
+	profile_list=sdp_list_append(0,&profile);
+	sdp_set_profile_descs(&record,profile_list);
 	//maketheservicerecordpubliclybrowsable
 	sdp_uuid16_create(&root_uuid,PUBLIC_BROWSE_GROUP);
-	root_list=sdplistappend(0,&root_uuid);
-	sdpsetbrowsegroups(&record,root_list);
+	root_list=sdp_list_append(0,&root_uuid);
+	sdp_set_browse_groups(&record,root_list);
 	//setl2capinformation
 	sdp_uuid16_create(&l2cap_uuid,L2CAP_UUID);
-	l2cap_list=sdplistappend(0,&l2cap_uuid);
-	proto_list=sdplistappend(0,l2cap_list);
+	l2cap_list=sdp_list_append(0,&l2cap_uuid);
+	proto_list=sdp_list_append(0,l2cap_list);
 	//registertheRFCOMMchannelforRFCOMMsockets
 	sdp_uuid16_create(&rfcomm_uuid,RFCOMM_UUID);
-	channel=sdpdataalloc(SDP_UINT8,&rfcomm_channel);
-	rfcomm_list=sdplistappend(0,&rfcomm_uuid);
-	sdplistappend(rfcomm_list,channel);
-	sdplistappend(proto_list,rfcomm_list);
-	access_proto_list=sdplistappend(0,proto_list);
-	sdpsetaccessprotos(&record,access_proto_list);
+	channel=sdp_data_alloc(SDP_UINT8,&rfcomm_channel);
+	rfcomm_list=sdp_list_append(0,&rfcomm_uuid);
+	sdp_list_append(rfcomm_list,channel);
+	sdp_list_append(proto_list,rfcomm_list);
+	access_proto_list=sdp_list_append(0,proto_list);
+	sdp_set_access_protos(&record,access_proto_list);
 	//setthename,provider,anddescription
-	sdpsetinfoattr(&record,service_name,service_prov,svc_dsc);
+	sdp_set_info_attr(&record,service_name,service_prov,svc_dsc);
 	//connecttothelocalSDPserver,registertheservicerecord,
 
 
 	
 	//anddisconnect
-	session=sdpconnect(BDADDR_ANY,BDADDR_LOCAL,SDP_RETRY_IF_BUSY);
-	sdprecordregister(session,&record,0);
+	session=sdp_connect(BDADDR_ANY,BDADDR_LOCAL,SDP_RETRY_IF_BUSY);
+	sdp_record_register(session,&record,0);
 	//cleanup
 	sdp_data_free(channel);
-	sdplistfree(l2cap_list,0);
-	sdplistfree(rfcomm_list,0);
-	sdplistfree(root_list,0);
-	sdplistfree(access_proto_list,0);
-	sdplistfree(svc_class_list,0);
-	sdplistfree(profile_list,0);
+	sdp_list_free(l2cap_list,0);
+	sdp_list_free(rfcomm_list,0);
+	sdp_list_free(root_list,0);
+	sdp_list_free(access_proto_list,0);
+	sdp_list_free(svc_class_list,0);
+	sdp_list_free(profile_list,0);
 	return session;
 	}
 int main()
